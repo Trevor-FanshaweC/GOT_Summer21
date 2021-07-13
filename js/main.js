ecf23ce623c8fad2566f5c1885231ae8f63ed96c
theMightyT <halvas_trevor@halvas.ca>
2021-07-13 13:07:31 -0400

    adding lightbox

---
+++ js/main.js
@@ -0,0 +1,18 @@
+(() => {
+  const sigils = document.querySelectorAll(".sigilContainer"),
+        lBox = document.querySelector(".lightbox"),
+        lbClose = lBox.querySelector(".close-button");
+
+  function popLightBox() {
+    // add the show-lightbox class to the lBox element
+    debugger;
+  }
+
+  function closeLightBox() {
+    // remove the show-lightbox class from the lBox element
+    debugger;
+  }
+
+  sigils.forEach(sigil => sigil.addEventListener("click", popLightBox));
+
+})();
