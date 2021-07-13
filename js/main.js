137e742842df39191fc095cfdff6def04643d8f8
theMightyT <halvas_trevor@halvas.ca>
2021-07-13 13:29:31 -0400

    lightbox is working

--- js/main.js
+++ js/main.js
@@ -5,14 +5,16 @@
 
   function popLightBox() {
     // add the show-lightbox class to the lBox element
-    debugger;
+    // debugger;
+    lBox.classList.add('show-lightbox');
   }
 
   function closeLightBox() {
     // remove the show-lightbox class from the lBox element
-    debugger;
+    lBox.classList.remove('show-lightbox');
+    // debugger;
   }
 
   sigils.forEach(sigil => sigil.addEventListener("click", popLightBox));
-
+  lbClose.addEventListener("click", closeLightBox);
 })();
